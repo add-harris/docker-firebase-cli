4dd1d0be7c9868737805234f7b82f6c92a6b5791
adam.harrison <adam.harrison1@dwp.gsi.gov.uk>
2020-11-06 11:56:59 +0000

    take BUILD_ID as a param

--- create-app.sh
+++ create-app.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+BUILD_ID=$1
+
 ## Create firebase app and capture output
 firebase apps:create WEB $BUILD_ID > output.txt
 
@@ -7,6 +9,8 @@ cat output.txt
 
 ## Extract App Id from output and write to file
 cat output.txt | grep "App ID:" | awk '{print $NF}' > appID.txt
+
+cat appID.txt
 
 ## Clean up
 rm output.txt
