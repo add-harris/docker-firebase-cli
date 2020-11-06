4f78f357b33478d760a003d42a3550659f318b90
adam.harrison <adam.harrison1@dwp.gsi.gov.uk>
2020-11-06 11:46:23 +0000

    added cat output.txt

--- create-app.sh
+++ create-app.sh
@@ -3,6 +3,8 @@
 ## Create firebase app and capture output
 firebase apps:create WEB $BUILD_ID > output.txt
 
+cat output.txt
+
 ## Extract App Id from output and write to file
 cat output.txt | grep "App ID:" | awk '{print $NF}' > appID.txt
 
