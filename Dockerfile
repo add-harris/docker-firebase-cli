0fdcc495eef787cd9700002f176d0043a38baf44
adam.harrison <adam.harrison1@dwp.gsi.gov.uk>
2020-11-06 10:15:15 +0000

    create-app.sh added

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,7 @@ FROM node
 
 RUN npm i -g firebase-tools
 
+COPY create-app.sh /create-app.sh
+RUN chmod 555 /create-app.sh
+
 ENTRYPOINT [ "firebase" ]
---
+++ create-app.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+## Create firebase app and capture output
+firebase apps:create WEB $BUILD_ID > output.txt
+
+## Extract App Id from output and write to file
+cat output.txt | grep "App ID:" | awk '{print $NF}' > appID.txt
+
+## Clean up
+rm output.txt
